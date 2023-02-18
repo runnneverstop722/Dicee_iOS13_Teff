57710c7237a99a06b1ae9e8553f5c10c617d255f
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-02-18 16:32:50 +0900

    Uploading the completed project

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // IBoutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        //'let' means constant in swift
+        let diceArray = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+            ]
+        
+        // randomElement() is also available
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+}
